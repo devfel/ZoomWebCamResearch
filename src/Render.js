1489dbf9968801aa293c3b297da25e7d80e0807e
devfel <devfelizardo@gmail.com>
2021-11-29 21:59:59 -0500

    updating paths to work the default app

--- src/Render.js
+++ src/Render.js
@@ -1,15 +1,16 @@
 import App from "./App";
 import AppSmallCam from "./AppSmallCam";
 import AppBigCam from "./AppBigCam";
-import {HashRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 
 function Render() {
   return (
     <Router>
       <Routes>
-        <Route path="/ZoomWebCamResearch/" element={<App/>}/>
-        <Route path="/ZoomWebCamResearch/small" element={<AppSmallCam/>}/>
-        <Route path="/ZoomWebCamResearch/big" element={<AppBigCam/>}/>
+        <Route exact path="/" element={<App/>}/>
+        <Route exact path="/ZoomWebCamResearch" element={<App/>}/>
+        <Route exact path="/small" element={<AppSmallCam/>}/>
+        <Route exact path="/ZoomWebCamResearch/big" element={<AppBigCam/>}/>
       </Routes>
     </Router>
   );
